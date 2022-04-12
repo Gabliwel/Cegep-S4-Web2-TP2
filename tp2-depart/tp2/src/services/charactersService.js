079344b095e219d67f92e00703876a376aa1cdc1
Gabriel Bertrand <gabriel_bertrand@hotmail.com>
2022-04-12 16:28:48 -0400

    ajout service des ennemy

---
+++ tp2-depart/tp2/src/services/charactersService.js
@@ -0,0 +1,17 @@
+import * as axios from 'axios'
+import { API } from '../../shared/config.js'
+
+async function getNbCharacters () {
+  const response = await axios.get(`${API}/characters?_page=1&_limit=1`)
+  return response.headers['x-total-count']
+}
+
+async function getCharacter (id) {
+  const response = await axios.get(`${API}/characters?_page=` + id + '&_limit=1')
+  return response.data
+}
+
+export const charactersService = {
+  getNbCharacters,
+  getCharacter
+}
